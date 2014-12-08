c76af393cf0a34b53390ee2d59a20a2960ba5f57
Tom Boettcher <tom.boettcher@cerner.com>
2014-12-08 14:57:19 -0600

    Issue 18: Declare Splunk service inside resource provider.

---
+++ definitions/splunk_service.rb
@@ -0,0 +1,15 @@
+# Declares the Splunk service resource, performing the specified action.
+define splunk_service, resource_name: nil, action: :nothing do
+  if platform_family?('windows')
+    service_name = 'SplunkForwarder'
+  else
+    service_name = 'splunk'
+  end
+
+  resource_name = params[:resource_name] || params[:name]
+  service resource_name do
+    service_name service_name
+    action params[:action]
+    supports status: true, start: true, stop: true, restart: true
+  end
+end
