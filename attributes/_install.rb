7a624295db4dda3a996e74b48569ebb04a25f3bf
Jeremy Miller <jeremy.j.miller@cerner.com>
2018-10-31 15:17:52 -0500

    Update version of Splunk to 7.0.4

--- attributes/_install.rb
+++ attributes/_install.rb
@@ -13,8 +13,8 @@ default['splunk']['external_config_directory'] =
     '/etc/splunk'
   end
 
-default['splunk']['package']['version'] = '7.0.3'
-default['splunk']['package']['build'] = 'fa31da744b51'
+default['splunk']['package']['version'] = '7.0.4'
+default['splunk']['package']['build'] = '68ba48f99743'
 
 default['splunk']['package']['base_url'] = 'https://download.splunk.com/products'
 default['splunk']['package']['platform'] = node['os']
