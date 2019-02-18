51fbd3a6f0630ef3b7eb99f9c9ab5be0770d7f92
SR <srikanthreddy5@yahoo.com>
2019-02-18 16:33:39 -0600

    Update _install.rb
    added space # and TODO

--- recipes/_install.rb
+++ recipes/_install.rb
@@ -87,7 +87,7 @@ run_command = "#{node['splunk']['cmd']} help commands --accept-license --answer-
 if Gem::Version.new(nsp['version']) >= Gem::Version.new('7.2.0')
   run_command += " --seed-passwd 'changeme'"
 end
-#TODO: Use admin_password from databag for splunk-first-run.
+# TODO: Use admin_password from databag for splunk-first-run.
 execute 'splunk-first-run' do
   command run_command
   user node['splunk']['user']
