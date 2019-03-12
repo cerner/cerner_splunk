7a714d890a6fee349541ebeed09e9d7ddfb7bc57
SR <srikanthreddy5@yahoo.com>
2019-03-11 20:53:16 -0500

    Update _install_spec.rb
    updated Splunk patch version 7.2.4.2

--- spec/unit/recipes/_install_spec.rb
+++ spec/unit/recipes/_install_spec.rb
@@ -40,7 +40,7 @@ describe 'cerner_splunk::_install' do
 
   let(:windows) { nil }
 
-  let(:splunk_file) { 'splunkforwarder-7.2.4-8a94541dcfac' }
+  let(:splunk_file) { 'splunkforwarder-7.2.4.2-fb30470262e3' }
   let(:splunk_filepath) { "/var/chef/cache/#{splunk_file}.txt" }
 
   before do
@@ -55,7 +55,7 @@ describe 'cerner_splunk::_install' do
     allow(File).to receive(:exist?).with('/opt/splunkforwarder/ftr').and_return(ftr_exists)
 
     allow(Dir).to receive(:glob).and_call_original
-    allow(Dir).to receive(:glob).with('/opt/splunkforwarder/splunkforwarder-7.2.4-8a94541dcfac-*').and_return(glob)
+    allow(Dir).to receive(:glob).with('/opt/splunkforwarder/splunkforwarder-7.2.4.2-fb30470262e3-*').and_return(glob)
 
     # Stub alt separator for windows in Ruby 1.9.3
     stub_const('::File::ALT_SEPARATOR', '/')
