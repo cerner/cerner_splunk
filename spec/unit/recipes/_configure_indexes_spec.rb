8b63a526aa58127beacbaee079aaee5e4b38b126
srikanthreddy5 <srikanthreddy5@yahoo.com>
2019-03-20 09:30:33 -0500

    modified the rspec as per the comments

--- spec/unit/recipes/_configure_indexes_spec.rb
+++ spec/unit/recipes/_configure_indexes_spec.rb
@@ -137,29 +137,29 @@ describe 'cerner_splunk::_configure_indexes' do
       expect(subject).to create_splunk_template('system/indexes.conf').with(expected_attributes)
     end
   end
-  # When _noGenerateTstatsHomePath => true in specific indexes, then writes the indexes.conf file without tstats for only for that indexes
-  context 'when _noGenerateTstatsHomePath is set to true' do
+
+  context 'when _noGenerateTstatsHomePath is set to true for specific indexes' do
     let(:index_config) do
       {
         'config' => {
           'volume:test' => {
             'path' => '/test/path'
           },
-          'index_a' => { '_directory_name' => 'foo', '_noGenerateTstatsHomePath' => true },
+          'index_a' => { '_volume' => 'bar', '_noGenerateTstatsHomePath' => true },
           'index_b' => { '_volume' => 'test', '_noGenerateTstatsHomePath' => true },
           'index_c' => { '_volume' => 'test' }
         }
       }
     end
 
-    it 'writes the indexes.conf file without tstats paths' do
+    it 'writes the indexes.conf file without tstatsHomePath only for those indexes' do
       expected_attributes = {
         stanzas: {
           'volume:test' => index_config['config']['volume:test'],
           'index_a' => {
-            'coldPath' => '$SPLUNK_DB/foo/colddb',
-            'homePath' => '$SPLUNK_DB/foo/db',
-            'thawedPath' => '$SPLUNK_DB/foo/thaweddb'
+            'coldPath' => 'volume:bar/index_a/colddb',
+            'homePath' => 'volume:bar/index_a/db',
+            'thawedPath' => '$SPLUNK_DB/index_a/thaweddb'
           },
           'index_b' => {
             'coldPath' => 'volume:test/index_b/colddb',
@@ -178,8 +178,8 @@ describe 'cerner_splunk::_configure_indexes' do
       expect(subject).to create_splunk_template('system/indexes.conf').with(expected_attributes)
     end
   end
-  # when _noGenerateTstatsHomePath => true in default stanza, then writes the indexes.conf file without tstats paths for all indexes
-  context 'when _noGenerateTstatsHomePath is set to true' do
+
+  context 'when _noGenerateTstatsHomePath is set to true in default stanza' do
     let(:index_config) do
       {
         'config' => {
@@ -189,22 +189,22 @@ describe 'cerner_splunk::_configure_indexes' do
           'volume:test' => {
             'path' => '/test/path'
           },
-          'index_a' => { '_directory_name' => 'foo' },
+          'index_a' => { '_volume' => 'bar' },
           'index_b' => { '_volume' => 'test' },
           'index_c' => { '_volume' => 'test' }
         }
       }
     end
 
-    it 'writes the indexes.conf file without tstats paths' do
+    it 'writes the indexes.conf file without tstats paths for all indexes' do
       expected_attributes = {
         stanzas: {
           'default' => {},
           'volume:test' => index_config['config']['volume:test'],
           'index_a' => {
-            'coldPath' => '$SPLUNK_DB/foo/colddb',
-            'homePath' => '$SPLUNK_DB/foo/db',
-            'thawedPath' => '$SPLUNK_DB/foo/thaweddb'
+            'coldPath' => 'volume:bar/index_a/colddb',
+            'homePath' => 'volume:bar/index_a/db',
+            'thawedPath' => '$SPLUNK_DB/index_a/thaweddb'
           },
           'index_b' => {
             'coldPath' => 'volume:test/index_b/colddb',
