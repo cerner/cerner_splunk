9706c8dc2d18fac131aa2a8b021a31ec75539d5e
Brett Graves <brett.graves@cerner.com>
2019-09-09 14:23:10 -0500

    Bump version after last release

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'splunk@cerner.com'
 license          'Apache-2.0'
 description      'Installs/Configures Splunk Servers and Forwarders'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.32.0'
+version          '2.33.0'
 
 source_url       'https://github.com/cerner/cerner_splunk'
 issues_url       'https://github.com/cerner/cerner_splunk/issues'
