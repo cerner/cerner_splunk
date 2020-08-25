66291f9077e102e399636401d3731513cd478ccd
Brett Graves <brett.graves@cerner.com>
2020-08-25 10:12:00 -0500

    Loosen version restriction on chef-vault (#222)
    * Fix a deprecation warning under Chef 16

--- metadata.rb
+++ metadata.rb
@@ -6,14 +6,14 @@ maintainer_email 'splunk@cerner.com'
 license          'Apache-2.0'
 description      'Installs/Configures Splunk Servers and Forwarders'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.40.0'
+version          '2.41.0'
 
 source_url       'https://github.com/cerner/cerner_splunk'
 issues_url       'https://github.com/cerner/cerner_splunk/issues'
 
 chef_version     '>= 12.7', '< 17'
 
-depends          'chef-vault', '~> 3.0'
+depends          'chef-vault', '> 3.0'
 depends          'ulimit', '~> 1.0'
 depends          'line', '~> 2.0'
 
