7f3cae9848d0464c7063ac4aca89465cbca9cb9f
Brett Graves <brett.graves@cerner.com>
2021-01-04 15:32:17 -0600

    Fix line cookbook dependency version

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'splunk@cerner.com'
 license          'Apache-2.0'
 description      'Installs/Configures Splunk Servers and Forwarders'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.42.0'
+version          '2.42.1'
 
 source_url       'https://github.com/cerner/cerner_splunk'
 issues_url       'https://github.com/cerner/cerner_splunk/issues'
@@ -15,7 +15,7 @@ chef_version     '>= 12.7', '< 17'
 
 depends          'chef-vault', '> 3.0'
 depends          'ulimit', '~> 1.0'
-depends          'line', '~> 2.0'
+depends          'line', '~> 2.1'
 
 supports         'redhat', '>= 6.7'
 supports         'ubuntu', '>= 12.04'
