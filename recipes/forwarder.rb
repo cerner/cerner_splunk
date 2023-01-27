860a5fecbb2160ece5f54230b95dec3a32dec141
hc068458 <henry.carr@cerner.com>
2023-01-27 11:58:21 -0600

    simplified false

--- recipes/forwarder.rb
+++ recipes/forwarder.rb
@@ -11,7 +11,7 @@ instance_exec :forwarder, &CernerSplunk::NODE_TYPE
 node.default['splunk']['package']['base_name'] = 'splunkforwarder'
 node.default['splunk']['package']['download_group'] = 'universalforwarder'
 
-fail 'Different Splunk artifact already installed on node. Failing as an unsupported install' if CernerSplunk.separate_splunk_installed?(node) && node['splunk']['ignore_already_installed_instance'] == false
+fail 'Different Splunk artifact already installed on node. Failing as an unsupported install' if CernerSplunk.separate_splunk_installed?(node) && !node['splunk']['ignore_already_installed_instance']
 
 ## Recipes
 include_recipe 'cerner_splunk::_install'
