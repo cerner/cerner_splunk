a40aa351ce7f8d5f26446eb3f3d57dc8b140b11e
hc068458 <henry.carr@cerner.com>
2023-02-06 10:09:54 -0600

    Fix splunk 6 not managed by systemd

--- recipes/_start.rb
+++ recipes/_start.rb
@@ -13,7 +13,8 @@ restart_flag = !(File.exist?(init_file_path) && File.readlines(init_file_path).g
 package_version = Gem::Version.new(node['splunk']['package']['version'])
 command = "#{node['splunk']['cmd']} enable boot-start -user #{node['splunk']['user']}"
 command += " -group #{node['splunk']['group']}" if package_version >= Gem::Version.new('7.3.0')
-command += " #{node['splunk']['boot_start_args']} -systemd-unit-file-name #{node['splunk']['systemd_unit_file_name']}" if package_version >= Gem::Version.new('7.2.2')
+command += " #{node['splunk']['boot_start_args']} -systemd-unit-file-name #{node['splunk']['systemd_unit_file_name']}" if package_version >= Gem::Version.new('7.2.2') if node['platform_version'].to_i != 6
+command += " #{node['splunk']['boot_start_args']}" if package_version >= Gem::Version.new('7.2.2') if node['platform_version'].to_i == 6
 
 execute command do
   not_if { platform_family?('windows') }
